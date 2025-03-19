8c0f69566f473311b72eef3e29bc2fbab915aa21
Ron Lobo <r.gonzalez@ronlobo.com>
2025-03-19 19:32:29 +0700

    Update base query example for query builder

--- indexer/sql/metrics/pool_tvl_by_latest_height.sql
+++ indexer/sql/metrics/pool_tvl_by_latest_height.sql
@@ -1,18 +1,10 @@
+/*
+    Select all pools latest token balances (by height) with pagination
+*/
 SELECT
-    p.pool_address,
-    p.height,
-    p.token0_denom,
-    p.token0_balance,
-    p.token1_denom,
-    p.token1_balance,
-    p.share,
-    (p.token0_balance * COALESCE(tp0.price, 0)) + (p.token1_balance * COALESCE(tp1.price, 0)) AS tvl
+    p.*
 FROM
     pool_balance p
-        LEFT JOIN
-    token_prices tp0 ON p.token0_denom = tp0.denomination
-        LEFT JOIN
-    token_prices tp1 ON p.token1_denom = tp1.denomination
         INNER JOIN (
         SELECT
             pool_address,
@@ -21,4 +13,36 @@ FROM
             pool_balance
         GROUP BY
             pool_address
-    ) latest ON p.pool_address = latest.pool_address AND p.height = latest.max_height;
+    ) latest ON p.pool_address = latest.pool_address AND p.height = latest.max_height
+ORDER BY p.pool_address -- Add an ORDER BY clause for consistent pagination
+LIMIT 100
+OFFSET 0;
+
+/*
+    Select pools based on pool address
+ */
+SELECT
+    p.*
+FROM
+    pool_balance p
+        INNER JOIN (
+        SELECT
+            pool_address,
+            MAX(height) AS max_height
+        FROM
+            pool_balance
+        WHERE
+            pool_address = ANY('{
+                "bbn10vzynuvh08kssssdrj9k2vaxxl9uqn0f08jaq8zq6h7vxmd9cnuqa3putu",
+                "bbn17xgsxm4vll7trsd59e26wg9f0unwmx2ktfhtvhu35jeel5wrakcqvnwzyu"
+            }'::text[])
+        GROUP BY
+            pool_address
+    ) latest ON p.pool_address = latest.pool_address AND p.height = latest.max_height
+WHERE
+    p.pool_address = ANY('{
+                "bbn10vzynuvh08kssssdrj9k2vaxxl9uqn0f08jaq8zq6h7vxmd9cnuqa3putu",
+                "bbn17xgsxm4vll7trsd59e26wg9f0unwmx2ktfhtvhu35jeel5wrakcqvnwzyu"
+            }'::text[])
+ORDER BY
+    p.pool_address;
