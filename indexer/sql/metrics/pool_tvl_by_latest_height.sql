188696b99dfd2bb2b0ec12ac6ef68d0b0fb78793
Ron Lobo <r.gonzalez@ronlobo.com>
2025-03-21 14:43:41 +0700

    Implement TVL metric, test and expose via tRPC

--- indexer/sql/metrics/pool_tvl_by_latest_height.sql
+++ indexer/sql/metrics/pool_tvl_by_latest_height.sql
@@ -1,6 +1,3 @@
-/*
-    Select all pools latest token balances (by height) with pagination
-*/
 SELECT
     p.*
 FROM
@@ -14,35 +11,6 @@ FROM
         GROUP BY
             pool_address
     ) latest ON p.pool_address = latest.pool_address AND p.height = latest.max_height
-ORDER BY p.pool_address -- Add an ORDER BY clause for consistent pagination
+ORDER BY p.pool_address
 LIMIT 100
 OFFSET 0;
-
-/*
-    Select pools based on pool address
- */
-SELECT
-    p.*
-FROM
-    pool_balance p
-        INNER JOIN (
-        SELECT
-            pool_address,
-            MAX(height) AS max_height
-        FROM
-            pool_balance
-        WHERE
-            pool_address = ANY('{
-                "bbn10vzynuvh08kssssdrj9k2vaxxl9uqn0f08jaq8zq6h7vxmd9cnuqa3putu",
-                "bbn17xgsxm4vll7trsd59e26wg9f0unwmx2ktfhtvhu35jeel5wrakcqvnwzyu"
-            }'::text[])
-        GROUP BY
-            pool_address
-    ) latest ON p.pool_address = latest.pool_address AND p.height = latest.max_height
-WHERE
-    p.pool_address = ANY('{
-                "bbn10vzynuvh08kssssdrj9k2vaxxl9uqn0f08jaq8zq6h7vxmd9cnuqa3putu",
-                "bbn17xgsxm4vll7trsd59e26wg9f0unwmx2ktfhtvhu35jeel5wrakcqvnwzyu"
-            }'::text[])
-ORDER BY
-    p.pool_address;
