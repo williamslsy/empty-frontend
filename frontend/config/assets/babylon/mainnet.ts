89bf005afd10ad542d1a5b6d296bfcb9a8cfecf7
Kyaris <45215699+kyar1s@users.noreply.github.com>
2025-04-10 14:42:18 +0200

    fix: update explorer (#117)
    * fix: update explorer
    
    * fix: union bridge
    
    * fix: make eureka default
    
    * fix: add token images

--- frontend/config/assets/babylon/mainnet.ts
+++ frontend/config/assets/babylon/mainnet.ts
@@ -16,7 +16,8 @@ export const BabylonMainnetAssets = {
     denom: "ibc/65D0BEC6DAD96C7F5043D1E54E54B6BB5D5B3AEC3FF6CEBB75B9E059F3580EA3",
     name: "USDC Noble",
     decimals: 6,
-    logoURI: "/assets/default.png",
+    logoURI:
+      "https://raw.githubusercontent.com/cosmos/chain-registry/master/_non-cosmos/ethereum/images/usdc.png",
     coingeckoId: "usd-coin",
   },
   "ibc/89EE10FCF78800B572BAAC7080AEFA301B5F3BBC51C5371E907EB129C5B900E7": {
@@ -25,7 +26,8 @@ export const BabylonMainnetAssets = {
     denom: "ibc/89EE10FCF78800B572BAAC7080AEFA301B5F3BBC51C5371E907EB129C5B900E7",
     name: "Lombard BTC",
     decimals: 8,
-    logoURI: "/assets/default.png",
+    logoURI:
+      "https://raw.githubusercontent.com/quasar-finance/quasar-resources/main/assets/coins/lbtc.svg",
     coingeckoId: "lombard-staked-btc",
   },
 } as unknown as Record<string, Currency>;
