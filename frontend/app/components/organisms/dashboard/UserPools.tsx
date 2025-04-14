a8bc231ff5729e84e4d5c954eebb994a66bddb6a
Kyaris <45215699+kyar1s@users.noreply.github.com>
2025-04-14 21:43:40 +0200

    fix: sort dashboard user pools

--- frontend/app/components/organisms/dashboard/UserPools.tsx
+++ frontend/app/components/organisms/dashboard/UserPools.tsx
@@ -60,83 +60,89 @@ export const UserPools: React.FC<Props> = ({ pools, isLoading, refreshUserPools 
   return (
     <Table columns={columns} gridClass={gridClass}>
       {isLoading && <PoolsDashboardSkeleton className={twMerge(gridClass)} />}
-      {pools.map(({ poolInfo, userBalance, incentives }, i) => {
-        return (
-          <TableRow key={i} gridClass={twMerge("flex flex-wrap lg:grid ", gridClass)}>
-            <CellPoolName
-              assets={poolInfo.assets}
-              name={poolInfo.name}
-              poolType={poolInfo.poolType}
-              config={poolInfo.config}
-              className="order-1 col-span-1 w-[80%] lg:w-auto"
-            />
-            <CellData title="APR" data="-" className="order-3 w-[45%] lg:w-auto" />
-            <CellDataToken
-              title="Staked"
-              poolAddress={poolInfo.poolAddress}
-              amount={userBalance.staked_share_amount}
-              tokens={poolInfo.assets}
-              className="order-4 w-[45%] lg:w-auto"
-            />
-            {/* <CellDataToken
+      {pools
+        .sort((a, b) =>
+          BigInt(a.userBalance.staked_share_amount) < BigInt(b.userBalance.staked_share_amount)
+            ? 1
+            : -1,
+        )
+        .map(({ poolInfo, userBalance, incentives }, i) => {
+          return (
+            <TableRow key={i} gridClass={twMerge("flex flex-wrap lg:grid ", gridClass)}>
+              <CellPoolName
+                assets={poolInfo.assets}
+                name={poolInfo.name}
+                poolType={poolInfo.poolType}
+                config={poolInfo.config}
+                className="order-1 col-span-1 w-[80%] lg:w-auto"
+              />
+              <CellData title="APR" data="-" className="order-3 w-[45%] lg:w-auto" />
+              <CellDataToken
+                title="Staked"
+                poolAddress={poolInfo.poolAddress}
+                amount={userBalance.staked_share_amount}
+                tokens={poolInfo.assets}
+                className="order-4 w-[45%] lg:w-auto"
+              />
+              {/* <CellDataToken
               title="Unstaked"
               poolAddress={poolInfo.poolAddress}
               amount={userBalance.unstaked_share_amount}
               tokens={poolInfo.assets}
               className="order-5 w-[45%] lg:w-auto"
             /> */}
-            <CellClaimRewards
-              rewards={incentives}
-              poolToken={userBalance.lpToken}
-              stakedAmount={userBalance.staked_share_amount}
-              className="order-6 w-[45%] lg:w-auto"
-            />
-            <div className="order-2 lg:order-7 flex items-end justify-end w-fit lg:w-auto">
-              <Menu>
-                <MenuButton className="mt-4 lg:mt-0">
-                  <IconDots className="w-6 h-6" />
-                </MenuButton>
+              <CellClaimRewards
+                rewards={incentives}
+                poolToken={userBalance.lpToken}
+                stakedAmount={userBalance.staked_share_amount}
+                className="order-6 w-[45%] lg:w-auto"
+              />
+              <div className="order-2 lg:order-7 flex items-end justify-end w-fit lg:w-auto">
+                <Menu>
+                  <MenuButton className="mt-4 lg:mt-0">
+                    <IconDots className="w-6 h-6" />
+                  </MenuButton>
 
-                <MenuItems
-                  transition
-                  className="min-w-[10rem] p-1 z-20 bg-tw-bg border border-white/10 rounded-lg"
-                  anchor="bottom end"
-                >
-                  <MenuItem>
-                    <button
-                      type="button"
-                      className="px-3 py-2 rounded-lg hover:text-tw-orange-400 hover:bg-tw-orange-400/20 w-full transition-all cursor-pointer"
-                      onClick={() =>
-                        showModal(ModalTypes.add_liquidity, false, {
-                          pool: poolInfo,
-                          refreshUserPools,
-                        })
-                      }
-                    >
-                      Add
-                    </button>
-                  </MenuItem>
-                  <MenuItem>
-                    <button
-                      type="button"
-                      className="px-3 py-2 rounded-lg hover:text-tw-orange-400 hover:bg-tw-orange-400/20 w-full transition-all cursor-pointer"
-                      onClick={() =>
-                        showModal(ModalTypes.remove_liquidity, false, {
-                          pool: poolInfo,
-                          balance: userBalance,
-                          refreshUserPools,
-                        })
-                      }
-                    >
-                      Remove
-                    </button>
-                  </MenuItem>
-                </MenuItems>
-              </Menu>
-            </div>
-          </TableRow>
-        );
-      })}
+                  <MenuItems
+                    transition
+                    className="min-w-[10rem] p-1 z-20 bg-tw-bg border border-white/10 rounded-lg"
+                    anchor="bottom end"
+                  >
+                    <MenuItem>
+                      <button
+                        type="button"
+                        className="px-3 py-2 rounded-lg hover:text-tw-orange-400 hover:bg-tw-orange-400/20 w-full transition-all cursor-pointer"
+                        onClick={() =>
+                          showModal(ModalTypes.add_liquidity, false, {
+                            pool: poolInfo,
+                            refreshUserPools,
+                          })
+                        }
+                      >
+                        Add
+                      </button>
+                    </MenuItem>
+                    <MenuItem>
+                      <button
+                        type="button"
+                        className="px-3 py-2 rounded-lg hover:text-tw-orange-400 hover:bg-tw-orange-400/20 w-full transition-all cursor-pointer"
+                        onClick={() =>
+                          showModal(ModalTypes.remove_liquidity, false, {
+                            pool: poolInfo,
+                            balance: userBalance,
+                            refreshUserPools,
+                          })
+                        }
+                      >
+                        Remove
+                      </button>
+                    </MenuItem>
+                  </MenuItems>
+                </Menu>
+              </div>
+            </TableRow>
+          );
+        })}
     </Table>
   );
 };
