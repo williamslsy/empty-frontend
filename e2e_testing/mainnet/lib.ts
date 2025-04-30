651fc405d12fa782772cdd858dd688d19f83dcd0
0xLaurens <32776056+0xLaurenzo@users.noreply.github.com>
2025-04-30 18:01:44 +0200

    fix deposit ratios   (#193)

--- e2e_testing/mainnet/lib.ts
+++ e2e_testing/mainnet/lib.ts
@@ -4,7 +4,7 @@ import {GasPrice} from "@cosmjs/stargate";
 import {stringToPath} from "@cosmjs/crypto";
 
 const rpcEndpoint = "https://babylon.nodes.guru:443/rpc";
-const mnemonic = "noise upon meadow minute purse utility first verb surprise blood debate dentist refuse spoon load panic demise celery strong nerve again word possible human";
+const mnemonic = "";
 const gasPrice = GasPrice.fromString("0.0025ubbn");
 const prefix = "bbn"
 // const hdPaths = [stringToPath("m/44'/118'/0'/0/0")]
