81beb80b3d08edb14732c41f29f53a30e70f13a8
Laurens <32776056+0xLaurenzo@users.noreply.github.com>
2025-05-20 13:52:40 +0200

    prune CellVolume

--- frontend/app/components/atoms/cells/CellVolume.tsx
+++ frontend/app/components/atoms/cells/CellVolume.tsx
@@ -3,28 +3,21 @@
 import type React from "react";
 import { twMerge } from "~/utils/twMerge";
 import { formatNumber } from "~/app/hooks/usePrices";
-import type { PoolMetricSerialized } from "@towerfi/types";
-import type { Currency } from "@towerfi/types";
-import type { Period } from "../PeriodToggle";
-import { useVolume } from "~/app/hooks/useVolume";
 
 interface Props {
   title: string;
-  metrics?: PoolMetricSerialized;
-  assets: Currency[];
+  volume: number;
   className?: string;
-  timeframe?: Period;
+  timeframe?: "7d" | "24h";
 }
 
-export const CellVolume: React.FC<Props> = ({ metrics, assets, className, timeframe = "7d" }) => {
-  const volume = useVolume(assets, metrics);
-
+export const CellVolume: React.FC<Props> = ({ volume, className, timeframe = "7d" }) => {
   return (
     <div className={twMerge("flex flex-col gap-2", className)}>
       <p className="text-xs text-white/50 lg:hidden">{`Volume ${timeframe === "7d" ? "7D" : "24h"}`}</p>
       <div className="flex items-center justify-between gap-3">
         <p>
-          {metrics
+          {volume
             ? `${formatNumber(volume, { currency: "USD", language: navigator.language })}`
             : "..."}
         </p>
