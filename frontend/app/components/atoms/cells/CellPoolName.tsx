21e15a95d71178ac030a61596c72ff0e9537f791
Laurens <32776056+0xLaurenzo@users.noreply.github.com>
2025-05-20 13:58:51 +0200

    prune CellPoolName

--- frontend/app/components/atoms/cells/CellPoolName.tsx
+++ frontend/app/components/atoms/cells/CellPoolName.tsx
@@ -1,21 +1,29 @@
 import type React from "react";
 import AssetsStacked from "../AssetsStacked";
-import type { PoolIncentive, PoolInfo } from "@towerfi/types";
 import { twMerge } from "~/utils/twMerge";
-import { PoolFeePill, PoolIncentivesPill, PoolTypePill } from "../PoolPill";
+import { PoolFeePill, PoolIncentivesPill, PoolTypePill, type PoolType, type Incentive } from "../PoolPill";
 
-interface Props extends Pick<PoolInfo, "assets" | "name" | "poolType" | "config"> {
+interface Token {
+  symbol: string;
+  logoURI: string;
+}
+
+interface Props {
+  assets: Token[];
+  name: string;
+  poolType: PoolType;
+  fee: number;
   className?: string;
-  incentives?: PoolIncentive;
+  incentive?: Incentive;
 }
 
 export const CellPoolName: React.FC<Props> = ({
   assets,
   name,
   poolType,
-  config,
+  fee,
   className,
-  incentives,
+  incentive,
 }) => {
   return (
     <div className={twMerge("col-span-2 lg:col-span-1 flex flex-col gap-2", className)}>
@@ -27,9 +35,9 @@ export const CellPoolName: React.FC<Props> = ({
         </div>
       </div>
       <div className="flex gap-1">
-        <PoolTypePill poolType={poolType} config={config} />
-        <PoolFeePill poolType={poolType} config={config} />
-        <PoolIncentivesPill incentives={incentives} />
+        <PoolTypePill poolType={poolType} />
+        <PoolFeePill fee={fee} />
+        {incentive && <PoolIncentivesPill incentive={incentive} />}
       </div>
     </div>
   );
