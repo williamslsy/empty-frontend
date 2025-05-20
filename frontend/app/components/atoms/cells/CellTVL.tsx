3dd58414a7131d7e2452dfd62a4216767b670f92
Laurens <32776056+0xLaurenzo@users.noreply.github.com>
2025-05-20 13:52:30 +0200

    prune CellTVL

--- frontend/app/components/atoms/cells/CellTVL.tsx
+++ frontend/app/components/atoms/cells/CellTVL.tsx
@@ -1,29 +1,24 @@
 "use client";
 
 import type React from "react";
-import type { PoolInfo } from "@towerfi/types";
 import { twMerge } from "~/utils/twMerge";
-import { useTVL } from "~/app/hooks/useTVL";
 import { formatNumber } from "~/app/hooks/usePrices";
 
-interface Props extends Pick<PoolInfo, "poolLiquidity" | "poolAddress" | "assets"> {
+interface Props {
+  tvl: number;
+  isLoading?: boolean;
   className?: string;
 }
 
-export const CellTVL: React.FC<Props> = ({ poolLiquidity, poolAddress, assets, className }) => {
-  const {
-    TVL,
-    query: { isLoading: isTVLLoading },
-  } = useTVL({ poolLiquidity, poolAddress, assets });
-
+export const CellTVL: React.FC<Props> = ({ tvl, isLoading, className }) => {
   return (
     <div className={twMerge("flex flex-col gap-2", className)}>
       <p className="text-xs text-white/50 lg:hidden">TVL</p>
-      <div className="flex items-center  justify-between gap-3">
+      <div className="flex items-center justify-between gap-3">
         <p>
-          {isTVLLoading
+          {isLoading
             ? "loading"
-            : formatNumber(TVL, {
+            : formatNumber(tvl, {
                 currency: "USD",
                 language: navigator.language,
               })}
